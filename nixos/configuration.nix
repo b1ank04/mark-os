a6d1f5febb3faa529701f4b7efb93873f1a235d2
Mark Shmarov <shmarov.mark@gmail.com>
2026-07-16 23:50:09 +0200

    update nvidia drivers

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -34,9 +34,9 @@
 
   hardware.nvidia = {
   	modesetting.enable = true;
-  	open = false;
+  	open = true;
   	nvidiaSettings = true;
-  	package = config.boot.kernelPackages.nvidiaPackages.production;
+  	package = config.boot.kernelPackages.nvidiaPackages.latest;
   };
 
   ###########################################
