42c408c329c7d93fd292ab0aea2ffe8945b9d875
Mark Shmarov <shmarov.mark@gmail.com>
2026-07-17 01:46:22 +0200

    aliases and cfg cleanup

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -84,21 +84,16 @@
     # Opinionated: disable channels
     channel.enable = false;
   };
+
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  # Networking
   networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
   networking.networkmanager.enable = true;
 
-  # Set your time zone.
+  ########## Timezone/Location ##########
   time.timeZone = "Europe/Berlin";
 
   # Select internationalisation properties.
@@ -116,10 +111,12 @@
     LC_TIME = "de_DE.UTF-8";
   };
 
-  # Enable the X11 windowing system.
-  # You can disable this if you're only using the Wayland session.
-  services.xserver.enable = true;
+  #######################################
 
+  # Desktop Environment #
+  
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
   # Enable the KDE Plasma Desktop Environment.
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
@@ -129,6 +126,8 @@
     layout = "us";
     variant = "";
   };
+
+  #######################
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
@@ -149,7 +148,6 @@
     #media-session.enable = true;
   };
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users."marks" = {
     isNormalUser = true;
     description = "Mark Shmarov";
@@ -172,6 +170,12 @@
 	telegram-desktop
 	wget
   ];
+
+  environment.shellAliases = {
+      nixrebuild = "sudo nixos-rebuild switch --flake ~/dotfiles/nixos#nixos";
+      nixboot = "sudo nixos-rebuild boot --flake ~/dotfiles/nixos#nixos";
+      nixclean = "sudo nix-collect-garbage -d";
+    };
   
-  system.stateVersion = "25.11"; # Did you read the comment?
+  system.stateVersion = "25.11";
 }
